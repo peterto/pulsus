429a0b3ae31cfebde42cc4a862a225e989cb0920
Peter To <pt9386@gmail.com>
2012-05-06 17:15:37 -0400

    Fixed rendered json controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,18 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @venues = Venue.all
+    response = { :user => @users, :venue => @venues }
 
     respond_to do |format|
       format.html
-      format.json { render json: @venues }
+      # format.json { render :json => { @users }}
+      # format.json  { render :json => response }
+      format.json { render :json => { :user => @users,
+                                       :venue => @venues } }
+      
+      # format.json { render :json => @venues }
+      format.text { render :text => response }
+
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @venues = Venue.all
+    # response = { :user => @users, :venue => @venues }
+
     respond_to do |format|
       format.html
-      format.json { render :json => @users }
-      format.text { render :text => @users }
+      # format.json { render :json => { @users }}
+      # format.json  { render :json => response }
+      # format.json { render :json => @venues }
+      format.json { render :json => { :user => @users,
+                                      :venue => @venues } }
+      format.text { render :text => response }
+
     end
   end
 
